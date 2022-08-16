a6b459f04b7773c797d7c27cf18225f9ac38b676
Jonas Olmstead <>
2022-08-16 16:11:03 +0200

    some art

--- src/js/bullet.js
+++ src/js/bullet.js
@@ -6,7 +6,7 @@ class Bullet extends EngineObject {
 		// your object init code here
 
 		this._lifetime = 30;
-		this._hitbox = vec2(1, 1);
+		this._hitbox = vec2(0.25);
 
 		this.timeAlive = 0;
 		this.setCollision(0, 0, 1);
@@ -15,11 +15,10 @@ class Bullet extends EngineObject {
 	update() {
 		// your object update code here
 
-		//if (isOverlapping(this.pos, this.size, player.pos, player.size))
 		for (let i = 0; i < g_game.enemies.length; i++) {
 			let enemy = g_game.enemies[i];
 			if (isOverlapping(this.pos, this._hitbox, enemy.pos, enemy._hitbox)) {
-				enemy.kill(this.velocity.copy());
+				enemy.kill(this.velocity.copy(), this.pos.copy());
 				this.destroy();
 				g_game.enemies.splice(i, 1);
 				i--;
--- src/js/enemy.js
+++ src/js/enemy.js
@@ -6,7 +6,7 @@ class Enemy extends EngineObject  {
 		// your object init code here
 		this._speed = 0.3;
 		this._walkCycleFrames = 30;
-		this._hitbox = vec2(2, 2);
+		this._hitbox = vec2(0.5);
 
 		this.tileIndex = 6;
 		this.setCollision(1, 1);
@@ -44,7 +44,37 @@ class Enemy extends EngineObject  {
  
 	}
 
-	kill(velocity) {
+	kill(velocity, pos) {
+
+		/*const angle = Math.atan2(velocity.y, velocity.x);
+
+		// create a particle emitter
+		let particleEmiter = new ParticleEmitter (
+			pos,					// pos
+			0,						// angle
+			1,						// emitSize
+			0,						// emitTime
+			500,					// emitRate
+			angle - Math.PI/6,  					// emiteCone
+			-1,						// tileIndex
+			vec2(12),  				// tileSize
+			new Color(1, 0, 0),		// colorStartA
+			new Color(1, 0, 0), 	// colorStartB
+			new Color(1, 0, 0),		//colorEndA
+			new Color(1, 0, 0), 	// colorEndB
+			2,						// particleTime
+			0.2,					// sizeStart
+			0.2,					// sizeEnd
+			0.1,					// particleSpeed
+			0.05,  					// particleAngleSpeed
+			0.99,					// damping
+			1,						// angleDamping
+			1,						// gravityScale
+			angle + Math.PI/6,						// particleCone
+			0.05,  					// fadeRate, 
+			0.5,						// randomness
+			1                // collide, additive, randomColorLinear, renderOrder
+		);*/
 
 		let corpse = new Corpse(this.pos.copy(), this.size.copy(), this.tileIndex, this.tileSize.copy());
 		corpse.push(velocity);
