025052c45d148c8eeae916a4529b16269c16e300
Jesper Rasmussen <jesper42@gmail.com>
2022-08-18 00:44:15 +0200

    zombies more stupid but faster changed gfx a little

--- src/js/mob.js
+++ src/js/mob.js
@@ -26,17 +26,18 @@ class Mob extends EngineObject {
 
 		let drag = speed * speed * dragConst;
 
+		if (drag > speed) drag = speed;
+
 		let dragForce = this.velocity.normalize(drag);
 
 		this.velocity = this.velocity.subtract(dragForce);
 	}
 
 	update() {
-
 		if (this.velocity.length() > 0.01) {
 			this.walkCyclePlace = (this.walkCyclePlace + 1) % this._walkCycleFrames;
 			this.mirror = this.walkCyclePlace > this._walkCycleFrames / 2 ? true : false;
-			this.bumpWalk = this.walkCyclePlace > this._walkCycleFrames / 2 ? 1/12 : 0;
+			this.bumpWalk = this.walkCyclePlace > this._walkCycleFrames / 2 ? 1 / 12 : 0;
 		} else {
 			this.walkCyclePlace = 0;
 			this.mirror = false;
@@ -46,7 +47,6 @@ class Mob extends EngineObject {
 	}
 
 	render() {
-
 		drawTile(
 			vec2(this.pos.x, this.pos.y + this.bumpWalk),
 			this.size,
@@ -56,16 +56,18 @@ class Mob extends EngineObject {
 			this.angle,
 			this.mirror
 		);
-
 	}
 
 	drawBlood() {
 		// blood
 		for (let i = 0; i < this.blood.length; i++) {
 			let blood = this.blood[i];
-			drawRect(vec2(this.pos.x + blood.x, this.pos.y + blood.y), vec2(1/12), new Color(172 / 255, 50 / 255, 50 / 255));
+			drawRect(
+				vec2(this.pos.x + blood.x, this.pos.y + blood.y),
+				vec2(1 / 12),
+				new Color(172 / 255, 50 / 255, 50 / 255)
+			);
 		}
-
 	}
 
 	hit(velocity, pos) {
@@ -76,7 +78,7 @@ class Mob extends EngineObject {
 		let radius = 0.5;
 
 		// prettier-ignore
-		this.bloodEmitter = new ParticleEmitter(
+		new ParticleEmitter(
 			this.pos, 0, radius/2, .02, 50*radius, PI, // pos, angle, emitSize, emitTime, emitRate, emiteCone
 			0, undefined,        // tileIndex, tileSize
 			new Color(.8,.1,.1), new Color(1,0,0), // colorStartA, colorStartB
@@ -99,7 +101,7 @@ class Mob extends EngineObject {
 		}
 		g_game.splatter.push(splatterPattern);
 
-		this.blood.push(vec2((this.pos.x - pos.x)/2, (this.pos.y - pos.y) / 2));
+		this.blood.push(vec2((this.pos.x - pos.x) / 2, (this.pos.y - pos.y) / 2));
 
 		return false;
 	}
