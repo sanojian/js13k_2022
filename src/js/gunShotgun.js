73e97b727f3ff7c6a3a10dc53a8467e64b400aee
Jesper Rasmussen <jesper42@gmail.com>
2022-08-21 10:52:58 +0200

    shotgun shoots 4 bullets

--- src/js/gunShotgun.js
+++ src/js/gunShotgun.js
@@ -6,11 +6,11 @@ class ShotGun extends Gun {
 		this._distance = 0.3;
 		this._mysize = size.y;
 		this._speed = 0.3;
-		this._maxAmmo = 4;
+		this._maxAmmo = 2;
 		this._ammoIconTile = g_game.tileNumbers.shellIcon;
 
 		this.ammo = this._maxAmmo;
-		this.reloadTimePerBullet = 0.75;
+		this.reloadTimePerBullet = 0.6;
 
 		this._soundFire = new Sound([2.45, , 352, 0.07, 0.01, 0.2, 4, 3.04, , 0.4, , , 0.15, 1.5, , 0.7, 0.12, 0.2]);
 	}
@@ -20,20 +20,30 @@ class ShotGun extends Gun {
 			var bulletColor = new Color(217 / 255, 87 / 255, 99 / 255);
 			var bulletLife = 12;
 
-			let angle = -this.angle;
-			let bullet = new Bullet(this.pos.copy(), 0, bulletColor, bulletLife);
-			bullet.velocity.x = Math.cos(angle) * this._speed;
-			bullet.velocity.y = Math.sin(angle) * this._speed;
+			const bullets = 4;
+			const spread = PI/6;
+			let angle = -this.angle - spread / 2;
+			for (let i = 0; i < bullets; i++) {
+				let bullet = new Bullet(this.pos.copy(), 0, bulletColor, bulletLife);
+				bullet.velocity.x = Math.cos(angle) * this._speed;
+				bullet.velocity.y = Math.sin(angle) * this._speed;
+				angle += spread / bullets;
+			}
 
-			angle = -this.angle + Math.PI / 24;
-			bullet = new Bullet(this.pos.copy(), 0, bulletColor, bulletLife);
-			bullet.velocity.x = Math.cos(angle) * this._speed;
-			bullet.velocity.y = Math.sin(angle) * this._speed;
+			// let angle = -this.angle;
+			// let bullet = new Bullet(this.pos.copy(), 0, bulletColor, bulletLife);
+			// bullet.velocity.x = Math.cos(angle) * this._speed;
+			// bullet.velocity.y = Math.sin(angle) * this._speed;
 
-			angle = -this.angle - Math.PI / 24;
-			bullet = new Bullet(this.pos.copy(), 0, bulletColor, bulletLife);
-			bullet.velocity.x = Math.cos(angle) * this._speed;
-			bullet.velocity.y = Math.sin(angle) * this._speed;
+			// angle = -this.angle + Math.PI / 24;
+			// bullet = new Bullet(this.pos.copy(), 0, bulletColor, bulletLife);
+			// bullet.velocity.x = Math.cos(angle) * this._speed;
+			// bullet.velocity.y = Math.sin(angle) * this._speed;
+
+			// angle = -this.angle - Math.PI / 24;
+			// bullet = new Bullet(this.pos.copy(), 0, bulletColor, bulletLife);
+			// bullet.velocity.x = Math.cos(angle) * this._speed;
+			// bullet.velocity.y = Math.sin(angle) * this._speed;
 		}
 		return true;
 	}
