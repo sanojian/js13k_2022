08ce03b120b7a8a9b5600bdc8bef88d2e658f42b
Jonas Olmstead <>
2022-08-26 18:40:24 +0200

    sounds for boss

--- src/js/boulder.js
+++ src/js/boulder.js
@@ -3,11 +3,14 @@
 class Boulder extends EngineObject {
 	constructor(pos, tileIndex) {
 		super(pos, vec2(1), tileIndex, TILE_SIZE, rand(-Math.PI / 8, Math.PI / 8), new Color(0.7, 0.7, 0.7));
+
+		this.soundDestroy = new Sound([1.08, , 50, , 0.18, 0.45, 4, 0, , , , , 0.01, 0.3, , 1.7, , 0.3, 0.11]);
 	}
 
 	collideWithTile() {
-		fx.shakeScreen(1);
+		fx.shakeScreen(0.5);
 
+		this.soundDestroy.play();
 		this.destroy();
 		return false;
 	}
