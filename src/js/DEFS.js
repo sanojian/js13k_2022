52c70320cadb7aba2ef4d5064b7dc9171cc9d465
Jesper Rasmussen <jesper42@gmail.com>
2022-09-03 09:32:33 +0200

    made game a bit easier

--- src/js/DEFS.js
+++ src/js/DEFS.js
@@ -99,7 +99,7 @@ const mobDefs = {
 	Ghost: {
 		hp: 1,
 		hpGainPerlevel: 0.5,
-		maxSpeed: 0.1,
+		maxSpeed: 0.08,
 	},
 };
 
@@ -114,22 +114,22 @@ const levelDefs = [
 	{
 		// graveyard
 		map: 1,
-		enemiesToSpawn: 15,
-		enemiesMaxAlive: 4,
+		enemiesToSpawn: 12,
+		enemiesMaxAlive: 3,
 		spawns: [{ chance: 1 }],
 	},
 	{
 		// intro to vampires
 		map: 3,
-		enemiesToSpawn: 20,
-		enemiesMaxAlive: 5,
+		enemiesToSpawn: 15,
+		enemiesMaxAlive: 4,
 		spawns: [{ chance: 0.5 }, { type: "v", chance: 0.5 }],
 	},
 	{
 		// intro to ghosts
 		map: 4,
-		enemiesToSpawn: 20,
-		enemiesMaxAlive: 5,
+		enemiesToSpawn: 15,
+		enemiesMaxAlive: 4,
 		spawns: [{ chance: 0.5 }, { type: "g", chance: 0.5 }],
 	},
 	{
