fe7caee6ca21e2f19b781d3a085316a763dbcc2b
Jesper Rasmussen <jesper42@gmail.com>
2022-09-06 20:45:52 +0200

    fixed bug

--- src/js/main.js
+++ src/js/main.js
@@ -40,7 +40,7 @@ function startNextLevel() {
 	let ammoPistol = g_player ? g_player.ammoBullets : 12;
 	let ammoShotgun = g_player ? g_player.ammoShells : 0;
 	let ammoRifle = g_player ? g_player.ammoRifle : 0;
-	let currentGun = g_player ? g_player.gun.tileIndex : tileNumbers_pistol; // default
+	let currentGun = g_player?.gun?.tileIndex ?? tileNumbers_pistol; // default
 	let gunAmmo = g_player ? g_player.gun.ammo : 6;
 
 	///////////////////
